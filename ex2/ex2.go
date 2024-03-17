b86e223720b7624c1e5156232d164cb793403650
Maykon Felix <equipefelix@gmail.com>
2024-03-17 17:31:28 -0300

    :construction: ex2 complete

--- ex2/ex2.go
+++ ex2/ex2.go
@@ -2,7 +2,26 @@ package main
 
 import "fmt"
 
+var x int
+var y string
+var z bool
+
 func main() {
-	fmt.Println("Testes")
+	fmt.Println(x, "\n", y, "\n", z)
 
 }
+
+/*
+- Use var para declarar trẽs variáveis. Elas devem ter package-level scope. Não atribua valores a estas variaveis. Utilize os seguintes identificadores e tipos para estas variáveis:
+1. Identificador "x" deverá ter tipo int
+2. Identificador "y" deverá ter tipo string
+3. Identificador "z" deverá ter tipo bool
+
+Na função main:
+1. Demonstre os valore de cada identificador
+2. o compilador atribui valores para essas variáveis. Como esses valores se chamam?
+! Se chamam
+? x = 0
+? y =
+? z = false
+*/
