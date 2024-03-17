61ae1be35e973461614c45f7edada5b57d4661ca
Maykon Felix <equipefelix@gmail.com>
2024-03-17 17:13:16 -0300

    :tada: fist commit

---
+++ ex1/ex1.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println("valores: ", x, y, z)
+
+	fmt.Println("valor X: ", x)
+	fmt.Println("valor y: ", y)
+	fmt.Println("valor z: ", z)
+
+}
+
+/*
+Utilizando o Operador curdo de declaração, atribua estes valores ás variaveis com os identificadores "x", "y", "z".
+1 - 42
+2 - "James Bond"
+3 - true
+Agora demonstra os valores nestas variávies com
+
+1 - Um única declaraçao println
+2 - Multimplas declarações print
+
+*/
