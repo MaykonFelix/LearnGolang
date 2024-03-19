e76b786a4686f733af7f0612f1073611b9a5ede6
MaykonFelix <equipefelixx@gmail.com>
2024-03-19 20:03:53 -0300

    :bricks: update ex4

--- ex4/ex4.go
+++ ex4/ex4.go
@@ -2,8 +2,23 @@ package main
 
 import "fmt"
 
-func main() {
+type fuinha int
 
-	var s string = "Hello World"
-	fmt.Printf("%s", s)
+var x fuinha
+
+func main() {
+	fmt.Println(x)
+
 }
+
+/*
+- Crie um tipo . O Tipo subjacente deve ser int.
+- Crie uma variável para este tipo, como o identificador "x", utilizando a palavra-chave var.
+- Na função main:
+
+	1. Demonstre o valor da variável "x"
+	2. Demonstre o tipo da variável "x"
+	3. Atribua 42 à variável "x" utilizando o operador "="
+	4. Demonstre o valor da variável "x"s
+
+*/
